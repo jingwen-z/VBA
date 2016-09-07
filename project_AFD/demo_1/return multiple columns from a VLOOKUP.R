c35c354049a64f9c2018cf0042d3a88fb64f9315
jingwen <jingwen7.zheng@gmail.com>
2016-09-07 10:53:39 +0200

    result one row for one lookup_value

---
+++ project_AFD/demo_1/return multiple columns from a VLOOKUP.R
@@ -0,0 +1,29 @@
+Sub display_result_in_a_new_wkb()
+'
+' return multiple columns from a VLOOKUP in a new workbook
+'
+    Dim wbkT1 As Workbook
+    Dim wbkOpenT2 As Workbook
+    Dim wbkRslt As Workbook
+    
+    Set wbkT1 = ThisWorkbook
+    ' open workbook "Arrête Comptable"
+    Set wbkOpenT2 = Workbooks.Open(wbkT1.Path & "\test2.xlsx")
+    
+    ' create a new workbook for reading result
+    Set wbkRslt = Workbooks.Add
+    
+    ' copy the fields' names from "test2"
+    wbkOpenT2.Sheets("t2_d1").Range("A1:E1").Copy
+    
+    ' paste to new workbook
+    wbkRslt.Sheets(1).Range("A1:E1").PasteSpecial
+        
+    ' select the cells (cells equal to the number of columns that you wish to
+    ' fetch) where you wish to populate the VLOOKUP results
+    wbkRslt.Sheets(1).Range("A2:E2").Select
+    
+    ' being to do VLOOKUP
+    Selection.FormulaArray = _
+        "=VLOOKUP([test1.xlsm]t1_d1!R2C1,[test2.xlsx]t2_d1!R2C1:R6C5,{1,2,3,4,5},0)"
+End Sub
